f61189e54c6523f14f95590cbd8e30946394b3f1
Junaid Mokit <junaid.rahman.mokit096@gmail.com>
2025-09-06 16:05:47 +0600

    add POST operation

--- Program.cs
+++ Program.cs
@@ -3,6 +3,8 @@ using GameStore.Api.Dtos;
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
+string GetGameEndPointName = "GetGame";
+
 List<GameDto> games = [
     new GameDto(1, "Elder Quest", "RPG", 59.99m, new DateOnly(2022, 5, 15)),
     new GameDto(2, "Speed Racers", "Racing", 39.99m, new DateOnly(2021, 11, 3)),
@@ -13,7 +15,22 @@ List<GameDto> games = [
 app.MapGet("games", () => games);
 
 // Get/games/1
-app.MapGet("games/{id}", (int id) => games.Find(game => game.Id == id));
+app.MapGet("games/{id}", (int id) => games.Find(game => game.Id == id))
+.WithName(GetGameEndPointName);
+
+app.MapPost("games", (CreateGameDto newGame) =>
+{
+    GameDto game = new(
+        games.Count + 1,
+        newGame.Name,
+        newGame.Genre,
+        newGame.Price,
+        newGame.ReleaseDate
+    );
+
+    games.Add(game);
+    return Results.CreatedAtRoute(GetGameEndPointName, new { id = game.Id }, game);
+});
 
 
 
--- obj/Debug/net9.0/GameStore.Api.AssemblyInfo.cs
+++ obj/Debug/net9.0/GameStore.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c87a12775ce26439c660b3b6bfc1a43196646f47")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f7dcd3867c1f0241d71bdfabed76ea815f22b4d9")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
